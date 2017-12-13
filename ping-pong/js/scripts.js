4963dae3ba78689f498bb0b9f4c4896faf0a2c68
George Gichimu <gichimumwai1@gmail.com>
2017-12-13 11:18:51 +0300

    changed user and business logics

--- ping-pong/js/scripts.js
+++ ping-pong/js/scripts.js
@@ -1,35 +1,29 @@
 //user interface
 $(document).ready(function(){
-  $(".submit").click(function(event){
-    event.preventDefault();
-    $("#result").empty();
-    var input = parseInt($("#userInput").val());
-    resultString = gameReturn(input);
-    $("#result").append(resultString);
-  });
+ $("form#userInput").submit(function(event){
+   event.preventDefault();
+   var pingpong= $("#result").val();
+   var result= userInput(pingpong);
+ result.forEach(function(element){
+   $("#reslut").append("<li>"+element);
+ })
+ });
 });
-
-
-var ping = function(aNumber){
-  return ping-pong;
-};
-}
-
-// Business Logic
-function gameReturn(num){
-    var resultArray = [];
-    var resultString = "";
-      for(var i = 1; i <= num; i++){
-      if((i % 15) === 0){
-        resultArray.push("<li>pingpong</li>");
-      }else if((i % 5) === 0){
-          resultArray.push("<li>pong</li>");
-        }else if((i %3) === 0){
-          resultArray.push("<li>ping</li>");
-        }else{
-          resultArray.push("<li>" + i + "</li>");
-    }
+//business logic
+function userInput(pingpong){
+ var result=[];
+ for(i=1; i<=pingpong;i++){
+   if (i%15===0){
+     result.push("ping pong");
+   }
+else if (i%5===0){
+   result.push("pong");
+ }else if (i%3===0){
+   result.push("ping");
+ }
+  else{
+    result.push(i);
   }
-  resultString = resultArray.join('');
-    return resultString;
+}
+return result;
 }
