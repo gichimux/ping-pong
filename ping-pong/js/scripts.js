a81c177223008337fd2d1cd9e01d58afb4ce26a7
George Gichimu <gichimumwai1@gmail.com>
2017-12-13 12:05:55 +0300

    re-structured js code, hope it works this time!

--- ping-pong/js/scripts.js
+++ ping-pong/js/scripts.js
@@ -1,29 +1,26 @@
 //user interface
-$(document).ready(function(){
- $("form#userInput").submit(function(event){
-   event.preventDefault();
-   var pingpong= $("#result").val();
-   var result= userInput(pingpong);
- result.forEach(function(element){
-   $("#reslut").append("<li>"+element);
- })
- });
-});
+$(document).ready(function() {
+ $("form#userInput").submit(function (event) {
+   $("#output ul").empty();
+   var aNumber = parseInt($("input").val());
+
+
 //business logic
-function userInput(pingpong){
- var result=[];
- for(i=1; i<=pingpong;i++){
-   if (i%15===0){
-     result.push("ping pong");
+ for(var i = 1; i<= aNumber; i += 1) {
+   if (i%15===0) {
+     $("#result ul").append('<li>' + "pingPong" + '</li>');
    }
-else if (i%5===0){
-   result.push("pong");
- }else if (i%3===0){
-   result.push("ping");
+else if (i%5===0) {
+      $("#result ul").append('<li>' + "pong" + '</li)');
+   }
+else if (i%3===0){
+      $("#result ul").append('<li>' + "ping" + '</li>');
  }
   else{
-    result.push(i);
+       $("#result ul" ).append('<li>' + i + '</li>');
   }
 }
-return result;
-}
+$("form#userInput")[0].reset();
+
+event.preventDefault();
+});
